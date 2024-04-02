f1a74afaca34d9c256f32ccbe258e27de6740fcc
arulh <arul.howard@telus.net>
2024-04-02 12:42:11 -0700

    Add impact calculations for different glow stick types

--- impact_calculator.js
+++ impact_calculator.js
@@ -1,9 +1,45 @@
+function multiplyMapValues(map, factor) {
+    let sum = 0;
+    map.forEach((value) => {
+      sum += value * factor;
+    });
+    return sum;
+  }
+
 function calculateAndDisplayWaste() {
     const numberOfGlowSticks = document.getElementById("numberOfGlowSticks").value;
-    const plasticWaste = numberOfGlowSticks * 15; // Assuming 15 grams of plastic waste per glow stick
-    const chemicalWaste = numberOfGlowSticks * 5; // Assuming 5 grams of chemical waste per glow stick
+
+    const csl_gwp = new Map([ // Cyulum snap lights global warming potential
+        ["ldpe", 0.0798],
+        ["h202", 0.01743],
+        ["glass", 0.1271646424]
+    ])
+
+    const csl_pec = new Map([ // Cyulum snap lights primary energy consumption
+        ["ldpe", 1.281],
+        ["h202", 0.307468],
+        ["glass", 0.0219]
+    ])
+
+    const lb_gwp = new Map([ // Lux bio glow global warming potential
+        ["pha", 0],
+        ["average_enzyme", 0.01446]
+    ])
+
+    const lb_pec = new Map([ // Lux bio glow primary energy consumption
+        ["pha", 0],
+        ["average_enzyme", 0.189]
+    ])
+
+    a = multiplyMapValues(csl_gwp, numberOfGlowSticks);
+    b = multiplyMapValues(csl_pec, numberOfGlowSticks);
+    c = multiplyMapValues(lb_gwp, numberOfGlowSticks);
+    d = multiplyMapValues(lb_pec, numberOfGlowSticks);
+
+    gwp_diff = a-c;
+    pec_diff = b-d;
 
     const resultDiv = document.getElementById("result");
-    resultDiv.innerHTML = `<p>Plastic Waste: ${plasticWaste} grams</p>
-                           <p>Chemical Waste: ${chemicalWaste} grams</p>`;
+    resultDiv.innerHTML = `<p>Carbon emissions reduced by: <b>${gwp_diff.toFixed(3)}</b> kg CO2</p>
+                           <p>Energy consumption reduced by: <b>${pec_diff.toFixed(3)}</b> MJ</p>`;
 }
